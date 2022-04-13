ac282b9de11534f3dba1446cd1da32a2696abbae
Chia Carlyle <chiacarlyle@gmail.com>
2022-04-14 00:17:17 +0100

    Use redux actions and the useDispatch method to add books to the UI

--- src/components/Books/Form/Form.js
+++ src/components/Books/Form/Form.js
@@ -1,10 +1,39 @@
-const Form = () => (
-  <form>
-    <h2>Add new book</h2>
-    <input type="text" placeholder="Book title" />
-    <input type="text" placeholder="Book author" />
-    <button type="button">Add Book</button>
-  </form>
-);
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../../Redux/Books/Books';
+
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const book = { title, author };
+        dispatch(addBook(book));
+        setTitle('');
+        setAuthor('');
+      }}
+    >
+      <h2>Add new book</h2>
+      <input
+        type="text"
+        placeholder="Book title"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        required
+      />
+      <input
+        type="text"
+        placeholder="Book author"
+        onChange={(e) => setAuthor(e.target.value)}
+        value={author}
+        required
+      />
+      <button type="submit">Add new</button>
+    </form>
+  );
+};
 
 export default Form;
