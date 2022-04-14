63df37396a543a3fe9245a125be1e477fe8fc607
Chia Carlyle <chiacarlyle@gmail.com>
2022-04-15 00:36:28 +0100

    Setup redux-thunk in the project

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,8 +1,9 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import categoriesReducer from './Categories/Categories';
 import booksReducer from './Books/Books';
 
 const rootReducer = combineReducers({ categoriesReducer, booksReducer });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
